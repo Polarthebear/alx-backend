9eb0bfcd1ae172a8051f36083b28b8ddce04724d
Polarthebear <thatotae325@gmail.com>
2024-11-05 21:37:07 +0000

    Task 4.

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -23,16 +23,21 @@ babel = Babel(app)
 @babel.localselector
 def get_locale():
     """Determines the best match with supported languages"""
-    locale = request.args.get('locale')
-    if locale:
-        return locale
+    queries = request.query_string.decode('utf-8').split('&')
+    query_table = dict(map(
+        lambda x: (x if '=' in x else '{}='.format(x)).split('='),
+        queries,
+    ))
+    if 'locale' in query_table:
+        if query_table['locale'] in app.config["LANGUAGES"]:
+            return query_table['locale']
     return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 @app.route('/', methods=['GET'])
 def index():
     """Hello World"""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 
 if __name__ == '__main__':
